770a2d323cd28c2eb3aa4da0989a1d235a3116e5
AmarMujkic <amar.mujkic@edu.fit.ba>
2024-05-06 12:35:49 +0200

    Add project files.

---
+++ eProdaja1/Controllers/ProizvodiController.cs
@@ -0,0 +1,24 @@
+﻿using eProdaja.Model;
+using eProdaja.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eProdaja1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProizvodiController : ControllerBase
+    {
+        private readonly IProizvodiService _proizvodiService;
+
+        public ProizvodiController(IProizvodiService proizvodiService)
+        {
+            _proizvodiService = proizvodiService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Proizvodi> Get()
+        {
+            return _proizvodiService.Get();
+        }
+    }
+}
